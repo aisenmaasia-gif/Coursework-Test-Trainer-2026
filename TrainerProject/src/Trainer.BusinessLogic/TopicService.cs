9c862c8370b34a62c3895b2f6f25a23b379e5c0a
[aisenmaasia-gif] <AisenmaAsia@gmail.com>
2026-05-04 15:29:08 +0300

    Business logic: TopicService added

---
+++ TrainerProject/src/Trainer.BusinessLogic/TopicService.cs
@@ -0,0 +1,36 @@
+using Trainer.Domain;
+using Trainer.DataAccess;
+
+namespace Trainer.BusinessLogic;
+
+public class TopicService
+{
+    private readonly DataContext _context;
+
+    public TopicService(DataContext context)
+    {
+        _context = context;
+    }
+
+    public void AddTopic(string name)
+    {
+        var topics = _context.Topics.GetAll().ToList();
+        if (topics.Any(t => t.Name == name)) throw new Exception("Тема вже існує");
+
+        topics.Add(new Topic {Name = name});
+        _context.Topics.SaveAll(topics);
+    }
+
+    public void AddQuestionToTopic(string topicName, Question question)
+    {
+        var topics = _context.Topics.GetAll().ToList();
+        var topic = topics.FirstOrDefault(t => t.Name == topicName);
+
+        if(topic == null) throw new Exception("Тему не знайдено");
+
+        topic.Questions.Add(question);
+        _context.Topics.SaveAll(topics);
+    }
+
+    public List<Topic> GetTopics() => _context.Topics.GetAll().ToList();
+}
